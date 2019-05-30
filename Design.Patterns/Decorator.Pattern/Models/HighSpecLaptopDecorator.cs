b861b965fd5f9f0f79c12be80cf693b082e014ae
Damon Hill <d.hill@ucas.ac.uk>
2019-05-30 15:15:18 +0100

    Decorator Pattern

--- Design.Patterns/Decorator.Pattern/Models/HighSpecLaptopDecorator.cs
+++ Design.Patterns/Decorator.Pattern/Models/HighSpecLaptopDecorator.cs
@@ -2,12 +2,10 @@ namespace Decorator.Pattern.Models
 {
     public class HighSpecLaptopDecorator : Computer
     {
-        private readonly Computer _computer;
-
         public HighSpecLaptopDecorator(Computer computer)
         {
             Description = computer.Description + ", High Spec";
-            Cost = 800 + _computer.Cost;
+            Cost = 800 + computer.Cost;
         }
 
         public override string Description { get; set; }
